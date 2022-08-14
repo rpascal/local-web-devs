a472bc6e418dc2a6cb8ad8f79bfffe5086e529fd
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-08-14 19:45:32 -0400

    fixed header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,14 @@
-<nav id="main-navbar">
-	<div class="navbar">
+<script lang="ts">
+	let scrollYAmount: number = 0;
+	// let fixedNavBar: boolean = scrollYAmount > 10
+</script>
+
+<svelte:window bind:scrollY={scrollYAmount} />
+
+<nav id="main-navbar" class={scrollYAmount > 10 ? 'header-fixed' : ''}>
+	<div class="navbar {scrollYAmount > 10 ? 'bg-primary-focus' : ''}" >
 		<div class="flex-1">
-			<a class="normal-case text-3xl m-4" href="/">
+			<a class="normal-case {scrollYAmount > 10 ? 'text-xl ml-4' : 'text-3xl m-4'}   " href="/">
 				<h1 class="font-bold">Local Web</h1>
 				<h1 class="font-light">Devs</h1>
 			</a>
@@ -33,8 +40,6 @@
 						<li><a href="/#section-why-us">Why Us</a></li>
 						<li><a href="/#section-services">Services</a></li>
 						<li><a href="/#section-contact">Contact</a></li>
-
-
 					</ul>
 				</div>
 			</div>
@@ -46,8 +51,6 @@
 				<li><a href="/#section-why-us">Why Us</a></li>
 				<li><a href="/#section-services">Services</a></li>
 				<li><a href="/#section-contact">Contact</a></li>
-
-
 			</ul>
 		</div>
 	</div>
@@ -58,12 +61,25 @@
 		position: absolute;
 		/* fixed to stick */
 		width: 100%;
-		/* 
+		
 		top: 0;
 		left: 0;
 		right: 0;
 		transition: padding 0.3s linear;
 		-webkit-transition: padding 0.3s linear;
-		z-index: 999; */
+		z-index: 999;
 	}
+
+	.header-fixed {
+		position: fixed !important;
+		left: 0;
+		right: 0;
+		top: 0;
+		background: #fff;
+		padding: 0 !important;
+		box-shadow: 0 0 4px rgba(0, 0, 0, 0.14), 0 4px 8px rgba(0, 0, 0, 0.28);
+		animation: 0.6s slideDown forwards;
+
+	}
+
 </style>
