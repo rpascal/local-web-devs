9d6854c9873b3f2cd8bfd0f9214934ebcf8a9248
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-08-22 19:56:26 -0400

    Pull contact form out to its own comp

--- src/components/ContactSection.svelte
+++ src/components/ContactSection.svelte
@@ -1,61 +1,6 @@
 <script lang="ts">
 	import ContactEmailAndPhoneVertical from './contact/ContactEmailAndPhoneVertical.svelte';
-
-	import { variables } from '$lib/variables';
-
-	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
-
-	import { createForm } from 'felte';
-	import { reporter, ValidationMessage } from '@felte/reporter-svelte';
-
-	import { validator } from '@felte/validator-zod';
-	import { z } from 'zod';
-
-	const schema = z.object({
-		name: z.string().min(1),
-		email: z.string().email().min(1),
-		message: z.string().min(1),
-		phone: z.string().min(1) //TODO validite phone number
-	});
-
-	function failedToast() {
-		toast.push('Error sending message, please try again later.', {
-			theme: {
-				'--toastBackground': '#F56565',
-				'--toastBarBackground': '#C53030'
-			}
-		});
-	}
-
-	const { form, isValid, reset } = createForm({
-		onSubmit: async (values) => {
-			try {
-				const res: Response = await fetch(`${variables.contactFormBasePath}/contact`, {
-					method: 'POST',
-					body: JSON.stringify({
-						replyToAddresses: [values.email],
-						toAddresses: [variables.contactEmail],
-						ccAddresses: [],
-						bccAddresses: [],
-						subject: `[Contact Request] Test Contact Form From App - ${values.name}`,
-						body: values.message
-					})
-				});
-
-				if (res.ok) {
-					reset();
-					toast.push('Message Sent');
-				} else {
-					console.error('Not ok response from contact form: ' + res.status);
-					failedToast();
-				}
-			} catch (e) {
-				console.error('Failed to send contact form email', e);
-				failedToast();
-			}
-		},
-		extend: [reporter, validator({ schema })]
-	});
+	import ContactForm from './contact/ContactForm.svelte';
 </script>
 
 <section id="section-contact" class="pt-20">
@@ -70,91 +15,7 @@
 			</div>
 
 			<div class="card md:col-span-2 w-full m-auto shadow-xl">
-				<form use:form>
-					<div class="card-body ">
-						<div class="form-control w-full ">
-							<label for="name-input" class="label">
-								<span class="label-text">Name</span>
-							</label>
-							<input
-								id="name-input"
-								type="text"
-								name="name"
-								placeholder="Enter Name"
-								class="input input-bordered w-full"
-							/>
-							<ValidationMessage for="name" let:messages>
-								<label class="label" for="name">
-									<span class="label-text-alt text-red-500">{messages?.[0] || ''}</span>
-								</label>
-							</ValidationMessage>
-						</div>
-
-						<div class="form-control w-full ">
-							<label for="email-input" class="label">
-								<span class="label-text">Email</span>
-							</label>
-							<input
-								id="email-input"
-								type="email"
-								name="email"
-								placeholder="Enter Email"
-								class="input input-bordered w-full"
-							/>
-
-							<ValidationMessage for="email" let:messages>
-								<label class="label" for="email">
-									<span class="label-text-alt text-red-500">{messages?.[0] || ''}</span>
-								</label>
-							</ValidationMessage>
-						</div>
-
-						<div class="form-control w-full ">
-							<label for="phone-input" class="label">
-								<span class="label-text">Phone Number</span>
-							</label>
-							<input
-								id="phone-input"
-								type="string"
-								name="phone"
-								placeholder="Enter Phone Number"
-								class="input input-bordered w-full"
-							/>
-
-							<ValidationMessage for="phone" let:messages>
-								<label class="label" for="phone">
-									<span class="label-text-alt text-red-500">{messages?.[0] || ''}</span>
-								</label>
-							</ValidationMessage>
-						</div>
-
-						<div class="form-control">
-							<label for="message-input" class="label">
-								<span class="label-text">Messgae</span>
-							</label>
-							<textarea
-								id="message-input"
-								type="text"
-								name="message"
-								class="textarea textarea-bordered h-24"
-								placeholder="Enter Message"
-							/>
-							<ValidationMessage for="message" let:messages>
-								<label class="label" for="message">
-									<span class="label-text-alt text-red-500">{messages?.[0] || ''}</span>
-								</label>
-							</ValidationMessage>
-						</div>
-
-						<!-- <div class="pt-2 w-full text-right">
-                            <button type="submit" disabled={!$isValid} class="btn btn-secondary">Send</button>
-                        </div> -->
-
-						<div class="card-actions justify-end">
-							<button type="submit" disabled={!$isValid} class="btn btn-secondary">Send</button>
-						</div>
-					</div>
-				</form>
+				<ContactForm />
 			</div>
 
 			<div class="hidden md:block">
@@ -163,14 +24,3 @@
 		</div>
 	</div>
 </section>
-
-<SvelteToast />
-
-<style>
-	:root {
-		--toastContainerTop: auto;
-		--toastContainerRight: auto;
-		--toastContainerBottom: 8rem;
-		--toastContainerLeft: calc(50vw - 8rem);
-	}
-</style>
--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,5 +1,6 @@
 export const variables = {
     contactFormBasePath: import.meta.env.VITE_CONTACT_FORM_BASE_PATH as string,
+    contactFormPostUrl: `${import.meta.env.VITE_CONTACT_FORM_BASE_PATH as string}/contact`,
     contactEmail: 'contact@localwebdevs.com',
     mailToContactEmail: 'mailto:contact@localwebdevs.com',
 };
