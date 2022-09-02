430fb46bb1a6879705721dd28ff35dd1bf5508c7
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-09-01 21:01:33 -0400

    Youtube iframe api myself and list old videos using search api

--- src/components/ServicesSection.svelte
+++ src/components/ServicesSection.svelte
@@ -1,33 +1,52 @@
 <section id="section-services" class="pt-20">
-	<div class="container mx-auto">
-		<div class="px-4 mx-auto max-w-6xl text-center items-center mb-10">
+	<div class="container mx-auto px-4 max-w-6xl text-center items-center grid grid-flow-row gap-10">
+		<div class="">
 			<p class="text-6xl text-primary font-light">Services</p>
 		</div>
 
-		<div class="grid grid-cols-1 md:grid-cols-4">
-			<div class="md:col-span-2 justify-center content-center m-auto">
-				<div class="card md:w-96  bg-base-100 shadow-xl mb-8">
-					<div class="card-body items-center text-center">
-						<h2 class="card-title text-primary">Standard</h2>
-						<p class="pb-4"><span class="text-5xl font-bold">$200</span>/month</p>
+		<div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-10 ">
+			<div class="card bg-base-100 shadow-xl  ">
+				<div class="card-body items-center text-center">
+					<h2 class="card-title text-primary">Basic</h2>
+					<p class="pb-4"><span class="text-5xl font-bold">$150</span>/month</p>
 
-						<p class="text-xl">Custom Design</p>
-						<div class="divider my-0" />
-						<p class="text-xl">Unlimited Edits</p>
-						<div class="divider my-0" />
-						<p class="text-xl">24/7 Support</p>
-						<div class="divider my-0" />
-						<p class="text-xl">Included Hosting</p>
-						<div class="divider my-0" />
+					<p class="text-xl">Single Page Design</p>
+					<div class="divider my-0" />
+					<p class="text-xl">4 Edits / Month</p>
+					<div class="divider my-0" />
+					<p class="text-xl">Included Hosting</p>
+					<div class="divider my-0" />
+					<p class="text-xl">Superior Support</p>
+					<div class="divider my-0" />
 
-						<div class="card-actions">
-							<button class="btn btn-primary">Contact</button>
-						</div>
+					<div class="card-actions">
+						<button class="btn btn-primary">Contact</button>
 					</div>
 				</div>
 			</div>
+			<div class="card bg-base-100 shadow-xl">
+				<div class="card-body items-center text-center">
+					<h2 class="card-title text-primary">Standard</h2>
+					<p class="pb-4"><span class="text-5xl font-bold">$200</span>/month</p>
 
-			<div class="md:col-span-2 px-5 md:pl-10">
+					<p class="text-xl">5 page Design</p>
+					<div class="divider my-0" />
+					<p class="text-xl">Unlimited Edits</p>
+					<div class="divider my-0" />
+					<p class="text-xl">Included Hosting</p>
+					<div class="divider my-0" />
+					<p class="text-xl">Superior Support</p>
+					<div class="divider my-0" />
+
+					<div class="card-actions">
+						<button class="btn btn-primary">Contact</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="grid grid-cols-1 mt-10">
+			<div class="">
 				<p class="text-4xl md:text-6xl font-light">
 					<span class="text-primary font-bold">$0</span> Down,
 					<span class="text-primary font-bold">$200</span> Per Month
@@ -52,8 +71,8 @@
 						<p>Included hosting so you don't need to worry about a thing</p>
 					</div>
 					<div>
-						<p class="text-2xl font-bold mb-2">24/7 Support</p>
-						<p>Call anytime day or night</p>
+						<p class="text-2xl font-bold mb-2">Superior Support</p>
+						<p>We will be ready to help for any issues that come up</p>
 					</div>
 					<div>
 						<p class="text-2xl font-bold mb-2">Analytics</p>
